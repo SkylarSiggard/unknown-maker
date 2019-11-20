98f3e1b598e4dd948431bf67824e83719666cf46
skylar siggard <siggardskylar@gmail.com>
2019-11-19 17:15:42 -0700

    it works but axios is super slow and some other small bugs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Unknown extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      unknowns: [],
+      name: []
+    }
+  }
+  componentDidMount() {
+    axios.get('https://pokeapi.co/api/v2/pokemon/unown/').then(res => {
+      this.setState({
+        unknowns: res.data.forms
+      })
+    })
+  }
+  handleChange = (e, key) => {
+    this.setState({
+        [key]: e.target.value
+    })
+}
+  handleEnter = (str) => {
+        for (let i = 0; i < str.length; i++) {
+          for(let j = 0; j < this.state.unknowns.length; j++) {
+            if (`unown-${str[i]}` === this.state.unknowns[j].name){
+              axios.get(`${this.state.unknowns[j].url}`).then(res => {
+                this.state.name.push({unown: res.data.sprites.front_default})
+              })
+            }
+          }
+        }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="inputs">
+        <input onChange={(e) => this.handleChange(e, 'input')} type="text" placeholder='Name'/>
+        <button onClick={() => this.handleEnter(this.state.input) }>Enter</button>
+        </div>
+        <div className="name-time">
+          {this.state.name.length ? (
+            this.state.name.map((el, i) => {
+              return (
+                <div className='items'>
+                  <img src={el.unown} alt="a pokemon"/>
+                </div>
+              )
+            })
+          ) : (
+            <h1>Input a name</h1>
+          )
+            }
+        </div>
     </div>
   );
 }
+}
 
-export default App;
+export default Unknown;
